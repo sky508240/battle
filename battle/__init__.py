9b1c93c6697e1fbb330e787d91f9dcef42b3f961
sky508240 <136697100+sky508240@users.noreply.github.com>
2025-09-17 22:15:06 +0200

    integrate fullbattlesystemcog

--- battle/__init__.py
+++ battle/__init__.py
@@ -3,9 +3,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
-# Import the battle cog
-from .battle_cog import Battle  # Replace 'battle_cog' with the actual filename of your Cog without .py
+from .cog import FullBattleSystemCog
 
 async def setup(bot: "BallsDexBot"):
-    """Setup function to add the Battle Cog."""
-    await bot.add_cog(Battle(bot))
+    """Setup function to add the FullBattleSystemCog."""
+    await bot.add_cog(FullBattleSystemCog(bot))
